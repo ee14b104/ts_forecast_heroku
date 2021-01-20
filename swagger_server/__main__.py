660605f36cb407476c204e2a036be5221656e915
ee14b104 <sachinagrawal272@gmail.com>
2021-01-20 23:42:01 +0100

    modifying port number

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -10,13 +10,9 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'sktime forecast'}, pythonic_params=True)
-    ON_HEROKU = os.environ.get('ON_HEROKU')
-    if ON_HEROKU:
-    # get the heroku port 
-        port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
-    else:
-        port = 8080
-    app.run(port=port)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host='0.0.0.0',port=port)
 
 
 if __name__ == '__main__':
